9f2ca673257ad611a070f9b32eafbace9313aaca
lef8 <alan.sipiorski14@gmail.com>
2023-06-04 22:47:10 +0200

    create login screen

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -3,14 +3,55 @@ package com.example.chatapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.colorspace.WhitePoint
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chatapp.MainActivity.InputType.Password.label
 import com.example.chatapp.ui.theme.ChatAppTheme
+import com.example.chatapp.ui.theme.EmperorDark
+import com.example.chatapp.ui.theme.Ivory
+import com.example.chatapp.ui.theme.Yellow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +63,78 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Login()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Preview
+    @Composable
+    private fun Login() {
+        Column(modifier = Modifier
+            .background(EmperorDark)
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+            TextInput(inputType = InputType.Username)
+            TextInput(InputType.Password)
+            Button(onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Yellow)
+            ) {
+                Text(text = "SIGN IN", Modifier.padding(vertical = 8.dp))
+            }
+            Divider(
+                color = Color.White,
+                thickness = 1.dp,
+                modifier = Modifier.padding(top = 48.dp)
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Don't have an account?", color = Ivory)
+                TextButton(onClick = {}) {
+                    Text(text = "SIGN UP", color = Yellow)
+                }
+            }
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChatAppTheme {
-        Greeting("Android")
+    sealed class InputType (
+        val label: String,
+        val icon: ImageVector,
+        val keyboardOptions: KeyboardOptions,
+        val visualTransformation: VisualTransformation
+        ) {
+        object Username : InputType(
+            label = "Username/E-mail",
+            icon = Icons.Default.Person,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            visualTransformation = VisualTransformation.None
+        )
+
+        object Password : InputType(
+            label = "Password",
+            icon = Icons.Default.Lock,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TextInput(inputType: InputType) {
+        var value by remember { mutableStateOf("") }
+        TextField(
+            value = value,
+            onValueChange = {value = it},
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            leadingIcon = { Icon(inputType.icon, contentDescription = "Icon") },
+            label = { Text(text = inputType.label) },
+            singleLine = true,
+            keyboardOptions = inputType.keyboardOptions,
+            visualTransformation = inputType.visualTransformation
+        )
     }
 }
