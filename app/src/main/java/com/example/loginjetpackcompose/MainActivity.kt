ec68388652da2edf04826e31cf764c9847d3d226
lef8 <alan.sipiorski14@gmail.com>
2023-06-09 23:00:33 +0200

    add on back press handler from sign up screen

--- app/src/main/java/com/example/loginjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/loginjetpackcompose/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.loginjetpackcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -10,41 +11,63 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
-import com.example.loginjetpackcompose.authentication.Authentication
+import com.example.loginjetpackcompose.authentication.FirebaseManager
 import com.example.loginjetpackcompose.presentation.LoggedIn
 import com.example.loginjetpackcompose.presentation.Login
+import com.example.loginjetpackcompose.presentation.ScreenState
+import com.example.loginjetpackcompose.presentation.SignUp
 import com.example.loginjetpackcompose.ui.theme.LoginJetpackComposeTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val authenticationService = FirebaseManager()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val isLoggedIn = remember { mutableStateOf(false) }
-            val authenticationService = Authentication()
+            val screenState = remember { mutableStateOf(ScreenState.LOGIN) }
+
             LoginJetpackComposeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    if (isLoggedIn.value) {
-                        LoggedIn(onLogoutClick = {
-                            isLoggedIn.value = false
-                            authenticationService.logout()
-                        })
-                    } else {
-                        Login(
-                            onLoginClick = { email, password ->
-                            lifecycleScope.launch {
-                                isLoggedIn.value = authenticationService.login(
-                                    email, password, this@MainActivity
-                                )
-                            }
-                        },
-                            onNoAccountClick = {
-                                // TODO("change screens")
-                            }
-                        )
+                    BackHandler(
+                        enabled = (screenState.value == ScreenState.SIGNUP) &&
+                                (screenState.value != ScreenState.LOGIN)) {
+                        screenState.value = ScreenState.LOGIN
+                    }
+
+                    when (screenState.value) {
+                        ScreenState.LOGIN -> {
+                            Login(
+                                onLoginClick = { email, password ->
+                                    lifecycleScope.launch {
+                                        isLoggedIn.value = authenticationService.login(
+                                            email, password, this@MainActivity
+                                        )
+                                        if (isLoggedIn.value) screenState.value = ScreenState.LOGGED_IN
+                                    }
+                                },
+                                onNoAccountClick = {
+                                    screenState.value = ScreenState.SIGNUP
+                                }
+                            )
+                        }
+
+                        ScreenState.SIGNUP -> {
+                            SignUp()
+                        }
+
+                        ScreenState.LOGGED_IN -> {
+                            LoggedIn(onLogoutClick = {
+                                isLoggedIn.value = false
+                                authenticationService.logout()
+                                screenState.value = ScreenState.LOGIN
+                            })
+                        }
                     }
                 }
             }
